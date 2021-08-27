2f5bebb7bc53e1e8ff8025ab643a777af3de0928
Julian Szigethy <julian.szigethy@gmail.com>
2021-08-27 17:35:29 +0200

    fixed typo

--- src/components/SkillCarousel.js
+++ src/components/SkillCarousel.js
@@ -58,7 +58,7 @@ export default function SkillCarousel() {
     const framerMotionObj = {
       title: "Framer Motion",
       details:
-        "Framer m is a powerful animation library that works hand-in-hand with react. It powers this carousel and many other things on this website.",
+        "Framer Motion is a powerful animation library that works hand-in-hand with react. It powers this carousel and many other things on this website.",
       background: "#B53471",
     };
 
